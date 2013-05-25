ae50f270c9ded1507726d14e58c5c74c34332220
technestudios <admin@technestudios.com>
2013-05-25 04:04:22 -0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -19,6 +19,39 @@
       <h3>Email: <input name="teacher_email" /></h3>
       <h3>School: <input name="school" /></h3>
       <h3>Desired username: <input name="teacher_username" /></h3>
+      <?php
+ 
+function generatePassword($length=9, $strength=0) {
+  $vowels = 'aeuy';
+	$consonants = 'bdghjmnpqrstvz';
+	if ($strength & 1) {
+		$consonants .= 'BDGHJLMNPQRSTVWXZ';
+	}
+	if ($strength & 2) {
+		$vowels .= "AEUY";
+	}
+	if ($strength & 4) {
+		$consonants .= '23456789';
+	}
+	if ($strength & 8) {
+		$consonants .= '@#$%';
+	}
+ 
+	$password = '';
+	$alt = time() % 2;
+	for ($i = 0; $i < $length; $i++) {
+		if ($alt == 1) {
+			$password .= $consonants[(rand() % strlen($consonants))];
+			$alt = 0;
+		} else {
+			$password .= $vowels[(rand() % strlen($vowels))];
+			$alt = 1;
+		}
+	}
+	return $password;
+}
+ 
+?>
       <input type="submit" />
     </form>
   </body>
